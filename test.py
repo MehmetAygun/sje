0a4f4ee57c074e87293ce0d0778e5d088e41d053
mehmet <mehmet@debian>
2016-08-02 00:27:43 +0300

    Create files

---
+++ test.py
@@ -0,0 +1,7 @@
+#Author : Mehmet Aygun
+
+"""
+For testing a new images using SJE Freamwork
+Usage : python test.py image1.jpg
+
+"""
---
+++ train.py
@@ -0,0 +1,9 @@
+#Author : Mehmet Aygun
+
+"""
+ SJE Training
+ It learns a compability function between  CNN features and attribute output label embeddings,
+ for fine-grained zero-shot image classification.
+ Used Dataset : Caltech-UCSD Birds-200-2011 Dataset
+
+"""
