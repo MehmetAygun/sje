83e083585628086eae7f2c42b2105c3d467342fb
MehmetAygun <mayguntr@gmail.com>
2016-08-30 22:55:34 +0300

    minor fix

--- test.py
+++ test.py
@@ -18,8 +18,10 @@ def argmax(input_embedding, W, output_embeddings):
     max_score = -100000
 
     W = np.matrix(W)
+
     projected_vector = np.dot(input_embedding, W)
-    # normalize
+
+    # Normalize projected vector
     projected_vector /= math.sqrt(np.dot(projected_vector, projected_vector.transpose()))
 
     for i in range(0, output_embeddings.shape[0]):
@@ -41,25 +43,30 @@ def get_accuracy (W,valid = True):
 
     if valid:
         h5file = h5py.File("/storage/mehmet/Zero-Shot/Features/valid.data", "r")
-        number_of_examples = 2607
+
     else:
         h5file = h5py.File("/storage/mehmet/Zero-Shot/Features/test.data", "r")
-        number_of_examples = 3691
+
 
     attributes = ra.read_attributes("/storage/mehmet/Zero-Shot/datasets/CUB_200_2011/CUB_200_2011/attributes/class_attribute_labels_continuous.txt")
     image_names = np.array(h5file['image_name'][:])
 
-    pb = Progbar(number_of_examples)
+
 
     # change class index to int value
     classes = []
 
+    features = np.array(h5file['pool5'][:])
+    number_of_examples = features.shape[0]
+    feature_dimension = features.shape[2]
+    features = features.reshape(number_of_examples, feature_dimension)  # 2048 feature dimension
+
     for i in range(0, number_of_examples):
         classes.append(int(h5file['class'][i][0]) * 100 + int(h5file['class'][i][1]) * 10 + int(h5file['class'][i][2]))
 
-    features = np.array(h5file['pool5'][:]).reshape(number_of_examples, 2048)  # 2048 feature dimension
     classes = np.array(classes)
 
+    pb = Progbar(number_of_examples)
     correct = 0
     for j in range(0,number_of_examples):
         y = argmax(features[j],W,attributes)
@@ -68,7 +75,6 @@ def get_accuracy (W,valid = True):
             correct = correct + 1
 
     return (correct / float(number_of_examples)) * 100
-
 
 if __name__ == '__main__':
     W = h5py.File("/storage/mehmet/Zero-Shot/Models/sje_w.data", "r")['W'][:]
--- train.py
+++ train.py
@@ -7,6 +7,7 @@
  Used Dataset : Caltech-UCSD Birds-200-2011 Dataset
 
 """
+from sklearn.preprocessing import normalize
 import numpy as np
 import read_attributes as ra
 import h5py
@@ -20,6 +21,9 @@ from keras.utils.generic_utils import Progbar
 RAND_MAX = 2147483647
 
 def rand_gen():
+    """
+    It create a random number and return that number.
+    """
     a = 0.0
     b = 0.0
 
@@ -36,32 +40,43 @@ def argmax(input_embedding,W,output_embeddings,correct_index):
             output_embeddings = number_of_classes X attribute_dimension
             W = n X attribute_dimension
 
-        """
+    """
     return_index = -1
     max_score = 0.0
 
-    W = np.matrix(W)
-    projected_vector = np.dot(input_embedding,W)
-    #normalize
-    projected_vector /= math.sqrt(np.dot(projected_vector,projected_vector.transpose()))
+    # Project image features on embedding matrix
+    #projected_vector = np.dot(input_embedding,W)
+    projected_vector = np.array(input_embedding).dot(np.matrix(W))
 
+    # Normalize projected vector
+    projected_vector = normalize(projected_vector.reshape(1,-1), axis=1,norm='l2')
+
+
+    #Compare projected vector for finding high compatability score
     for i in range(0,output_embeddings.shape[0]):
-        score = np.dot(projected_vector, output_embeddings[i]) # dot product similarity
+        # dot product similarity
+        score = np.dot(projected_vector, output_embeddings[i])
+        #if image correct label is false add a margin
         if i != correct_index:
-            cost = 1.0
+            cost = 5.0
         else:
             cost = 0.0
-        score += cost #margin
+
+        score = score + cost #margin
         if score > max_score:
             max_score = score
             return_index = i
 
+    #return index that create maximum compatibility score
     return return_index
 
 if __name__ == '__main__':
 
-    #read attributes 200x312
+    #read attributes
     attributes = ra.read_attributes("/storage/mehmet/Zero-Shot/datasets/CUB_200_2011/CUB_200_2011/attributes/class_attribute_labels_continuous.txt")
+
+    number_of_classes = attributes.shape[0]
+    attributes_dimension = attributes.shape[1]
 
     #read features
     h5file_train = h5py.File("/storage/mehmet/Zero-Shot/Features/train.data", "r")
@@ -69,52 +84,62 @@ if __name__ == '__main__':
     # 5490 item
     train_image_name = np.array(h5file_train['image_name'][:])
 
+    number_of_images_train = train_image_name.size
     #change class index to int value
     train_class = []
 
-    random_index = [[i] for i in range(5490)]
+    #create random index list for training
+    random_index = [[i] for i in range(number_of_images_train)]
+    shuffle(random_index)
 
-    for i in range(0,5490):
+    #change class indexes string to integer
+    for i in range(0,number_of_images_train):
         train_class.append(int(h5file_train['class'][i][0])*100 + int(h5file_train['class'][i][1])*10 +int(h5file_train['class'][i][2]))
-    train_features = np.array(h5file_train['pool5'][:]).reshape(5490,2048)#2048 feature dimension
+
+    #read image features
+    train_features = np.array(h5file_train['pool5'][:])
+    train_features_dimension = train_features.shape[2]
+    train_features = train_features.reshape(number_of_images_train,train_features_dimension)
+
+
     train_class = np.array(train_class)
 
-
-
     #Parameters that should be validate
-    learning_rate = 1e-5
+    learning_rate = 1e-7
     max_epoch = 20
     lda = 1e-63
-    #initialize W
 
-    std_dev = 1.0 / math.sqrt(312)
 
-    W =np.zeros((2048,312))# np.random.random_sample(2048*312)# 2048:feature dim,312:a output embedding dimension
-    for i in range(2048):
+    #Randomly initialize embedding Matrix W
+
+    std_dev = 1.0 / math.sqrt(attributes_dimension)
+    W =np.zeros((train_features_dimension,attributes_dimension))
+    for i in range(train_features_dimension):
         dot = 0
-        for j in range(312):
+        for j in range(attributes_dimension):
             W[i][j] = std_dev * rand_gen()
-            dot += (W[i][j])*(W[i][j])
-        W[i] *= (lda/dot)
+        W[i] = normalize(W[i].reshape(1,-1),axis=1,norm='l2')
 
+    # W_best hold the weights that have best accuracy on validation set
     W_best = np.copy(W)
     best_accuracy = 0
 
+    #Start training
     for i in range(0,max_epoch):
         number_of_true = 0
         print "Epoch " + str(i)
-        pb = Progbar(500)
-        shuffle(random_index)
-        for j in random_index[0:500]:
+        pb = Progbar(number_of_images_train)
 
+        for j in random_index:
             pb.add(1)
             y = argmax(train_features[j],W,attributes,train_class[j]-1)
-            if (train_class[j]-1) != y and y!=-1: #make update
-                W = W + np.dot(np.transpose(train_features[j]),learning_rate * (attributes[train_class[j]-1]-attributes[y]))
-            else:
+            if (train_class[j]-1) != y and y!=-1: # if wrong predicton make update
+                W = W + np.dot(np.transpose(train_features[j]), (attributes[train_class[j]-1]-attributes[y])) * learning_rate
+            elif (train_class[j]-1) == y:
                 number_of_true += 1
         print "Number of true " + str(number_of_true)
 
+        #Get validation accuracy
         print "Testing : "
         accuracy = test.get_accuracy(W,valid=True)
         print "Epoch " + str(i) + "accuracy is :" + str(accuracy)
